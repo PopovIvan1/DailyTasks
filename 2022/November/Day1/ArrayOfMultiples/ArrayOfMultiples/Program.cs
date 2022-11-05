f919faf6d895c0f6db96ae872f41a7e03f995d16
Popov Ivan <bottob79@mail.ru>
2022-11-05 17:24:49 +0300

    Added solution of first task

--- 2022/November/Day1/ArrayOfMultiples/ArrayOfMultiples/Program.cs
+++ 2022/November/Day1/ArrayOfMultiples/ArrayOfMultiples/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArrayOfMultiples
 {
     class Program
     {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
         static void Main(string[] args)
         {
+            Console.WriteLine(string.Join(" ", ArrayOfMultiples(4, 7)));
+            Console.WriteLine(string.Join(" ", Enumerable.Range(1, 7).ToArray()));
+            Console.ReadLine();
+        }
+
+        public static int[] ArrayOfMultiples(int num, int length)
+        {
+            return Enumerable.Range(1, length).Select(x => x * num).ToArray();
         }
     }
 }
