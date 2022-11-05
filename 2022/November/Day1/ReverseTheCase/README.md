ec560d07abd6bb41f5cfc32548b503ee6ee2a24d
Popov Ivan <bottob79@mail.ru>
2022-11-05 18:04:35 +0300

    Added task number 2

---
+++ 2022/November/Day1/ReverseTheCase/README.md
@@ -0,0 +1,9 @@
+Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+
+Examples:
+
+ReverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+
+ReverseCase("MANY THANKS") ➞ "many thanks"
+
+ReverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
---
+++ 2022/November/Day1/ReverseTheCase/ReverseTheCase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseTheCase
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseCase("Happy Birthday"));
+            Console.ReadLine();
+        }
+
+        public static string ReverseCase(string str)
+        {
+            return string.Concat(str.Select(ch => char.IsUpper(ch) ? char.ToLower(ch) : char.ToUpper(ch)));
+        }
+    }
+}
