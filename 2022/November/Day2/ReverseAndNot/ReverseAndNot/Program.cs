f739aae9f114f6db6a260dfb00a3d247055a509b
Popov Ivan <bottob79@mail.ru>
2022-11-06 17:03:24 +0300

    Added task 1 in day 2

---
+++ 2022/November/Day2/ReverseAndNot/ReverseAndNot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndNot
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseAndNot(3456));
+            Console.ReadLine();
+        }
+
+        public static string ReverseAndNot(int i)
+        {
+            return new string(i.ToString().Reverse().ToArray()) + i;
+        }
+    }
+}
