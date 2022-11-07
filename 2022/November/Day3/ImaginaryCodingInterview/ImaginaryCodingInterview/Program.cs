201f147958378f448b9e9cc2c92f192e677538bd
Popov Ivan <bottob79@mail.ru>
2022-11-07 19:12:46 +0300

    Added first task in day 3

---
+++ 2022/November/Day3/ImaginaryCodingInterview/ImaginaryCodingInterview/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ImaginaryCodingInterview
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Interview(new int[] { 5, 5, 10, 10, 15, 15, 20, 20 }, 120));
+            Console.ReadLine();
+        }
+
+        public static string Interview(int[] arr, int tot)
+        {
+            var timeLimits = new int[] { 5, 5, 10, 10, 15, 15, 20, 20 };
+            return arr.Zip(timeLimits, (a, b) => a <= b).Count(s => s) == 8 && tot <= 120
+                ? "qualified"
+                : "disqualified";
+        }
+    }
+}
