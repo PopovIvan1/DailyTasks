5483cd976f0cc62c14cbbe04857be35cb2ff386a
Popov Ivan <bottob79@mail.ru>
2022-11-08 17:09:18 +0300

    Added first task in day 4

---
+++ 2022/November/Day4/NextPrime/NextPrime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace NextPrime
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NextPrime(34));
+            Console.ReadLine();
+        }
+
+        public static int NextPrime(int num)
+        {
+            return Enumerable.Range(2, (int)Math.Sqrt(num) + 1).All(x => num % x != 0) ? num : NextPrime(num + 1);
+        }
+    }
+}
