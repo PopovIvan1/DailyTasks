c68fb56a38df7c5ffb968814d5f5b07e19df5dc2
Popov Ivan <bottob79@mail.ru>
2022-11-08 18:22:34 +0300

    Added second task in day 4

---
+++ 2022/November/Day4/TrackTheRobot/TrackTheRobot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace TrackTheRobot
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", TrackRobot(new string[] { "right 10", "up 50", "left 30", "down 10" })));
+            Console.ReadLine();
+        }
+
+        public static int[] TrackRobot(string[] instructions)
+        {
+            int x = 0, y = 0;
+
+            instructions.Sum(instruction =>
+                instruction.Contains("right") ? x += int.Parse(instruction.Split(' ')[1]) :
+                instruction.Contains("left") ? x -= int.Parse(instruction.Split(' ')[1]) :
+                instruction.Contains("up") ? y += int.Parse(instruction.Split(' ')[1]) :
+                y -= int.Parse(instruction.Split(' ')[1]));
+
+            return new int[] { x, y };
+        }
+    }
+}
