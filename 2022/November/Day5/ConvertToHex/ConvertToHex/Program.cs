a33e21181b648eb282a1e91f8762ed4712c5320e
Popov Ivan <bottob79@mail.ru>
2022-11-09 17:10:45 +0300

    Day 5, task 1

---
+++ 2022/November/Day5/ConvertToHex/ConvertToHex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConvertToHex
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertToHex("hello world"));
+            Console.ReadLine();
+        }
+
+        public static string ConvertToHex(string inputword)
+        {
+            return string.Join(" ", inputword.Select(ch => ((int)ch).ToString("x2")));
+        }
+    }
+}
---
+++ 2022/November/Day5/ConvertToHex/README.md
@@ -0,0 +1,14 @@
+Create a function that takes a string's characters as ASCII and returns each character's hexadecimal value as a string.
+
+Examples
+
+ConvertToHex("hello world") ➞ "68 65 6c 6c 6f 20 77 6f 72 6c 64"
+
+ConvertToHex("Big Boi") ➞ "42 69 67 20 42 6f 69"
+
+ConvertToHex("Marty Poppinson") ➞ "4d 61 72 74 79 20 50 6f 70 70 69 6e 73 6f 6e"
+
+Notes
+
+Each byte must be seperated by a space.
+All alpha hex characters must be lowercase.
