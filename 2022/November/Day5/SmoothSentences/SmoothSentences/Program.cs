00cac6e43e303ce0aa027048c2fec45d1b7e6281
Popov Ivan <bottob79@mail.ru>
2022-11-09 17:34:53 +0300

    Day 5, task 2

---
+++ 2022/November/Day5/SmoothSentences/SmoothSentences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SmoothSentences
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSmooth("She eats super righteously"));
+            Console.ReadLine();
+        }
+
+        public static bool IsSmooth(string sentence)
+        {
+            string[] words = sentence.ToLower().Split(' ');
+            for (int i = 0; i < words.Length - 1; i++)
+                if (words[i][words[i].Length - 1] != words[i + 1][0])
+                    return false;
+            return true;
+        }
+    }
+}
