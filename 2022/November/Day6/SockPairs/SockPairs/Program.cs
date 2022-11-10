55992a5e97feaa35ca9bf1616bee9719fe67ec29
Popov Ivan <bottob79@mail.ru>
2022-11-10 17:29:07 +0300

    Day 6, task 1

---
+++ 2022/November/Day6/SockPairs/SockPairs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace SockPairs
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SockPairs("hello world"));
+            Console.ReadLine();
+        }
+
+        public static int SockPairs(string socks)
+        {
+            return socks.ToCharArray().GroupBy(c => c).Select(c => c.Count() / 2).Sum();
+        }
+    }
+}
