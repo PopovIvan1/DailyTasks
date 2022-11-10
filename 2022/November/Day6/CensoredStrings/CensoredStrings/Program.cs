510d564f2c3119dddec184318703b19286dabb56
Popov Ivan <bottob79@mail.ru>
2022-11-10 18:16:45 +0300

    Day 6, task 2

---
+++ 2022/November/Day6/CensoredStrings/CensoredStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CensoredStrings
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo"));
+            Console.ReadLine();
+        }
+
+        public static string Uncensor(string txt, string vowels)
+        {
+            var i = 0;
+            return Regex.Replace(txt, @"\*", _ => vowels[i++].ToString());
+        }
+    }
+}
