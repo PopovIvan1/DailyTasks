0661afba0a9a4ed89a672adcb0a6664bad183114
Popov Ivan <bottob79@mail.ru>
2022-11-11 17:39:58 +0300

    Day 7, task 2

---
+++ 2022/November/Day7/ConsecutiveNumbers/ConsecutiveNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsecutiveNumbers
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cons(new[] { 5, 1, 4, 3, 2 }));
+            Console.ReadLine();
+        }
+
+        public static bool Cons(int[] arr)
+        {
+            Array.Sort(arr);
+            int curVal = arr[0] - 1;
+            return arr.All(x => x == ++curVal);
+        }
+    }
+}
---
+++ 2022/November/Day7/ConsecutiveNumbers/README.md
@@ -0,0 +1,10 @@
+﻿Create a function that determines whether elements in an array can be re-arranged to form a consecutive list of numbers where each number appears exactly once.
+
+Examples
+Cons([5, 1, 4, 3, 2]) ➞ true
+// Can be re-arranged to form [1, 2, 3, 4, 5]
+
+Cons([5, 1, 4, 3, 2, 8]) ➞ false
+
+Cons([5, 6, 7, 8, 9, 9]) ➞ false
+// 9 appears twice
