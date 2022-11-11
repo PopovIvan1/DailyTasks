3028c6cc2c40d7281caf50afa69209e1581b0ad9
Popov Ivan <bottob79@mail.ru>
2022-11-11 16:51:56 +0300

    Day 7, task 1

---
+++ 2022/November/Day7/CupSwapping/CupSwapping/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CupSwapping
+{
+    class Program
+    {
+        /// <summary>
+        /// Function usage example.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CupSwapping(new[] { "AB", "CA", "AB" }));
+            Console.ReadLine();
+        }
+
+        public static string CupSwapping(string[] swaps)
+        {
+            return swaps.Aggregate("B",(curr, move) =>
+                  move.Contains(curr) ? move.Replace(curr, "") : curr);
+        }
+    }
+}
